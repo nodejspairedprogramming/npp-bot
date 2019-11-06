b4aa3012989e8a94b66dbd58122dfa5524fdff0c
Jean Felisme <jeazy@bizdotsmedia.com>
2019-11-05 20:38:55 -0500

    base files or start files

---
+++ index.js
@@ -0,0 +1,16 @@
+// require the dotENV
+const dotENV = require('dotenv').config();
+// require the discord.js module
+const Discord = require('discord.js');
+
+// create a new Discord client
+const client = new Discord.Client();
+
+// when the client is ready, run this code
+// this event will only trigger one time after logging in
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+// login to Discord with your app's token
+client.login(process.env.DISCORD_TOKEN);
